2b66a7045668f777d1341ae7597bfc84e63c5f6b
nishibayashi sakine <s.n.baton319@icloud.com>
2016-08-04 12:11:47 +0900

    Initial Commit

---
+++ Noutore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noutore
+//
+//  Created by 西林咲音 on 2016/08/04.
+//  Copyright © 2016年 西林咲音. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
