a3d7bb8dfc45cd5c9e5fdea2a0807addb3b43bb9
nishibayashi sakine <s.n.baton319@icloud.com>
2016-08-10 16:50:09 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+  
+target 'Noutore' do
+  pod 'Firebase/Core'
+  pod 'Firebase/Database’
+
+end
---
+++ Pods/Target Support Files/Pods-Noutore/Pods-Noutore-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_NoutoreVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NoutoreVersionString[];
+
